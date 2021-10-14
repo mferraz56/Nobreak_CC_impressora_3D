ddde861a9c00e20d3936f4467689b32223a426f8
Murilo <34348884+mferraz56@users.noreply.github.com>
2021-10-14 20:04:24 -0300

    Simulação

--- Simulação/Codigo/Codigo.ino
+++ Simulação/Codigo/Codigo.ino
@@ -1,9 +1,31 @@
 
  
-void setup() {
+int MosfetFonte = 4;
+int MosfetBateria = 2;
+int TensaoFonte = 9;
+int TensaoCarga = 8;
+int VFonte = 0;
 
+void setup(){
+pinMode(MosfetFonte, OUTPUT); // declara o pino do Mosfet fonte como saída
+pinMode(MosfetBateria, OUTPUT); // declara o pino do Mosfet fonte saída
+pinMode(TensaoFonte, INPUT);     // DECLARA ENTRADA
+pinMode(TensaoCarga, INPUT);   // DECLARA ENTRADA
+//digitalWrite(MosfetBateria,LOW);
+//digitalWrite(MosfetFonte,HIGH);
 }
  
-void loop() {
+void loop(){
+VFonte =  analogRead(TensaoFonte);
+if(VFonte <= 680)
+{
+    digitalWrite(MosfetBateria, HIGH);
+    digitalWrite(MosfetFonte, LOW);
+} 
+if(VFonte > 680)
+{
+    digitalWrite(MosfetBateria, LOW);
+    digitalWrite(MosfetFonte, HIGH);
+}
 
 }
