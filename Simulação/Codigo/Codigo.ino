686fb45bac509560e66e809d42d04ec32586f000
Murilo <34348884+mferraz56@users.noreply.github.com>
2021-10-23 15:11:03 -0300

    att

--- Simulação/Codigo/Codigo.ino
+++ Simulação/Codigo/Codigo.ino
@@ -1,31 +1,76 @@
+unsigned long tempo_anterior = 0; // variavel
+const long intervalo = 5000;      //Intervalo 5 seg
 
- 
-int MosfetFonte = 4;
-int MosfetBateria = 2;
-int TensaoFonte = 9;
-int TensaoCarga = 8;
-int VFonte = 0;
+int LedAzul = 4;
+int LedVerde = 5;
+int LedAmarelo = 6;
+int LedVermelho = 7;
+int VFonte; // variavel tensão fonte
+int VBateria;
 
-void setup(){
-pinMode(MosfetFonte, OUTPUT); // declara o pino do Mosfet fonte como saída
-pinMode(MosfetBateria, OUTPUT); // declara o pino do Mosfet fonte saída
-pinMode(TensaoFonte, INPUT);     // DECLARA ENTRADA
-pinMode(TensaoCarga, INPUT);   // DECLARA ENTRADA
-//digitalWrite(MosfetBateria,LOW);
-//digitalWrite(MosfetFonte,HIGH);
-}
- 
-void loop(){
-VFonte =  analogRead(TensaoFonte);
-if(VFonte <= 680)
+void setup()
 {
-    digitalWrite(MosfetBateria, HIGH);
-    digitalWrite(MosfetFonte, LOW);
-} 
-if(VFonte > 680)
-{
-    digitalWrite(MosfetBateria, LOW);
-    digitalWrite(MosfetFonte, HIGH);
+    pinMode(2, OUTPUT); // mosfet bateria
+    pinMode(3, OUTPUT); // mosfet fonte
+    pinMode(A0, INPUT); // leitura bateria
+    pinMode(A1, INPUT); // leitura fonte
+
+    pinMode(4, OUTPUT); // saida led azul
+    pinMode(5, OUTPUT); // saida led verde
+    pinMode(6, OUTPUT); // saida led amarelo
+    pinMode(7, OUTPUT); // saida led vermelho
+
+    //digitalWrite(2,LOW);
+    //digitalWrite(3,HIGH);
 }
 
+void loop()
+{
+    VFonte = analogRead(A1);
+
+    if (VFonte <= 700)
+    {
+        digitalWrite(2, HIGH);
+        digitalWrite(3, LOW);
+    }
+    if (VFonte > 700)
+    {
+        digitalWrite(2, LOW);
+        digitalWrite(3, HIGH);
+    }
+
+    unsigned long timer = millis(); // inicia em 0 até o infinito
+    if ((timer - tempo_anterior) == intervalo)
+    {
+        tempo_anterior = timer; //armazenando o tempo anterior
+        VBateria = analogRead(A0);
+        if (VBateria <= 777)
+        {
+            digitalWrite(LedVermelho, HIGH);
+            digitalWrite(LedAmarelo, LOW);
+            digitalWrite(LedVerde, LOW);
+            digitalWrite(LedAzul, LOW);
+        }
+        if (VBateria > 777 && VBateria <= 847)
+        {
+            digitalWrite(LedVermelho, LOW);
+            digitalWrite(LedAmarelo, HIGH);
+            digitalWrite(LedVerde, LOW);
+            digitalWrite(LedAzul, LOW);
+        }
+        if (VBateria > 847 && VBateria <= 918)
+        {
+            digitalWrite(LedVermelho, LOW);
+            digitalWrite(LedAmarelo, LOW);
+            digitalWrite(LedVerde, HIGH);
+            digitalWrite(LedAzul, LOW);
+        }
+        if (VBateria > 918 && VBateria <= 1023)
+        {
+            digitalWrite(LedVermelho, LOW);
+            digitalWrite(LedAmarelo, LOW);
+            digitalWrite(LedVerde, LOW);
+            digitalWrite(LedAzul, HIGH);
+        }
+    }
 }
